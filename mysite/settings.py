a12997d557a7afac8fed472516b885fee0f379d0
Ajith Menon <40847889+AjithMenonIN@users.noreply.github.com>
2019-06-20 22:55:27 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g=m_4!vm5ex%2f@ihchjt$aq)df#nkzr6o)+gy6mg$pwu!bl$s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ajithmenonin.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
