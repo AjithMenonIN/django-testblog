350f0eceb91e132f32cc9ea74ab77f5c5d8ad5e5
Ajith Menon <40847889+AjithMenonIN@users.noreply.github.com>
2019-06-20 22:50:03 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g=m_4!vm5ex%2f@ihchjt$aq)df#nkzr6o)+gy6mg$pwu!bl$s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 
 # Application definition
